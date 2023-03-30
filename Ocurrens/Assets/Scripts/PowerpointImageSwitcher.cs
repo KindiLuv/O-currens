b54e9d6df6bb41849eb061879f539659376a340d
kindiluv <anykennylster@gmail.com>
2023-03-30 17:06:51 +0200

    started cycling pipoint

--- Ocurrens/Assets/Scripts/PowerpointImageSwitcher.cs
+++ Ocurrens/Assets/Scripts/PowerpointImageSwitcher.cs
@@ -1,18 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PowerpointImageSwitcher : MonoBehaviour
 {
+    [SerializeField] GameObject displayHud;
+    [SerializeField] List<RawImage> _pipoint;
+    private RawImage activeRawImage;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        activeRawImage.enabled = true;
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Show/Hide the UI
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            
+        }
+        
+        if (displayHud.activeInHierarchy)
+        {
+           // Cycle thru rawImages
+           if (Input.GetKeyDown(KeyCode.P))
+           {
+               activeRawImage.enabled = false;
+               switch (activeRawImage)
+               {
+                   case not null when activeRawImage == _pipoint[0]:
+                       activeRawImage = _pipoint[1];
+                       break;
+                   case not null when activeRawImage == _pipoint[1]:
+                       activeRawImage = _pipoint[2];
+                       break;
+                   case not null when activeRawImage == _pipoint[2]:
+                       activeRawImage = _pipoint[0];
+                       break;
+               }
+               activeRawImage.enabled = true;
+           } 
+        }
         
     }
 }
