825ef680b154cbc37674559c2188499f498c3450
Pierre Durand <85309955+TotallyNotAB0t@users.noreply.github.com>
2023-03-30 18:40:08 +0200

    Test

--- Ocurrens/Assets/MultiplayerControlManager.cs
+++ Ocurrens/Assets/MultiplayerControlManager.cs
@@ -1,13 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using Photon.Pun;
+using StarterAssets;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class MultiplayerControlManager : MonoBehaviour
 {
     [SerializeField] private List<GameObject> myObjects;
     [SerializeField] private GameObject myCapsule;
     [SerializeField] private PhotonView myphotonView;
+
+    [SerializeField] private GameObject ControllerToDisable;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,6 +22,11 @@ public class MultiplayerControlManager : MonoBehaviour
                 obj.SetActive(true);
             }
             myCapsule.SetActive(false);
+            Destroy(ControllerToDisable.GetComponent<CharacterController>());
+            Destroy(ControllerToDisable.GetComponent<FirstPersonController>());
+            Destroy(ControllerToDisable.GetComponent<StarterAssetsInputs>());
+            Destroy(ControllerToDisable.GetComponent<BasicRigidBodyPush>());
+            Destroy(ControllerToDisable.GetComponent<PlayerInput>());
         }
     }
 
