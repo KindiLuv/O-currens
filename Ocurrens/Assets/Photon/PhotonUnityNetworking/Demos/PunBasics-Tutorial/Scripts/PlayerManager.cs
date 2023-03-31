dd5bf8e3cd2aac13d97e0cc0c4b75126aeede1da
Pierre Durand <85309955+TotallyNotAB0t@users.noreply.github.com>
2023-03-31 08:33:17 +0200

    wip emote multi

--- Ocurrens/Assets/Photon/PhotonUnityNetworking/Demos/PunBasics-Tutorial/Scripts/PlayerManager.cs
+++ Ocurrens/Assets/Photon/PhotonUnityNetworking/Demos/PunBasics-Tutorial/Scripts/PlayerManager.cs
@@ -1,6 +1,16 @@
-﻿using System;
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="PlayerManager.cs" company="Exit Games GmbH">
+//   Part of: Photon Unity Networking Demos
+// </copyright>
+// <summary>
+//  Used in PUN Basics Tutorial to deal with the networked player instance
+// </summary>
+// <author>developer@exitgames.com</author>
+// --------------------------------------------------------------------------------------------------------------------
+
 using System.Collections;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 namespace Photon.Pun.Demo.PunBasics
 {
@@ -10,16 +20,35 @@ namespace Photon.Pun.Demo.PunBasics
     /// Player manager.
     /// Handles fire Input and Beams.
     /// </summary>
-    public class PlayerManager : MonoBehaviourPunCallbacks
+    public class PlayerManager : MonoBehaviourPunCallbacks, IPunObservable
     {
         #region Public Fields
 
-        [SerializeField] private GameObject Canvass;
         [SerializeField] private GameObject OkImage;
         [SerializeField] private GameObject WrongImage;
 
+        [Tooltip("The current Health of our player")]
+        public float Health = 1f;
+
         [Tooltip("The local player instance. Use this to know if the local player is represented in the Scene")]
         public static GameObject LocalPlayerInstance;
+
+        #endregion
+
+        #region Private Fields
+
+        [Tooltip("The Player's UI GameObject Prefab")]
+        [SerializeField]
+        private GameObject playerUiPrefab;
+
+        [Tooltip("The Beams GameObject to control")]
+        [SerializeField]
+        private GameObject beams;
+
+        //True, when the user is firing
+        bool IsFiring;
+        bool IsOkImage;
+        bool isWrongImage;
 
         #endregion
 
@@ -30,6 +59,18 @@ namespace Photon.Pun.Demo.PunBasics
         /// </summary>
         public void Awake()
         {
+            if (this.beams == null)
+            {
+                Debug.LogError("<Color=Red><b>Missing</b></Color> Beams Reference.", this);
+            }
+            else
+            {
+                this.beams.SetActive(false);
+            }
+            
+            OkImage.SetActive(false);
+            WrongImage.SetActive(false);
+
             // #Important
             // used in GameManager.cs: we keep track of the localPlayer instance to prevent instanciation when levels are synchronized
             if (photonView.IsMine)
@@ -41,7 +82,10 @@ namespace Photon.Pun.Demo.PunBasics
             // we flag as don't destroy on load so that instance survives level synchronization, thus giving a seamless experience when levels load.
             DontDestroyOnLoad(gameObject);
         }
-        
+
+        /// <summary>
+        /// MonoBehaviour method called on GameObject by Unity during initialization phase.
+        /// </summary>
         public void Start()
         {
             CameraWork _cameraWork = gameObject.GetComponent<CameraWork>();
@@ -57,17 +101,44 @@ namespace Photon.Pun.Demo.PunBasics
             {
                 Debug.LogError("<Color=Red><b>Missing</b></Color> CameraWork Component on player Prefab.", this);
             }
-            /*OkImage.SetActive(false);
-            WrongImage.SetActive(false);*/
         }
 
-        private void Update()
+        private bool leavingRoom;
+
+        public void Update()
         {
+            // we only process Inputs and check health if we are the local player
             if (photonView.IsMine)
             {
-                ProcessInputs();
+                this.ProcessInputs();
+
+                if (this.Health <= 0f && !this.leavingRoom)
+                {
+                    this.leavingRoom = GameManager.Instance.LeaveRoom();
+                }
+            }
+
+            if (this.beams != null && this.IsFiring != this.beams.activeInHierarchy)
+            {
+                this.beams.SetActive(this.IsFiring);
+            }
+
+            if (OkImage != null && IsOkImage != OkImage.activeInHierarchy)
+            {
+                OkImage.SetActive(IsOkImage);
+            }
+            
+            if (WrongImage != null && isWrongImage != WrongImage.activeInHierarchy)
+            {
+                WrongImage.SetActive(isWrongImage);
             }
         }
+
+        public override void OnLeftRoom()
+        {
+            this.leavingRoom = false;
+        }
+
         #endregion
 
         #region Private Methods
@@ -77,24 +148,83 @@ namespace Photon.Pun.Demo.PunBasics
         /// </summary>
         void ProcessInputs()
         {
+            if (Input.GetButtonDown("Fire1"))
+            {
+                // we don't want to fire when we interact with UI buttons for example. IsPointerOverGameObject really means IsPointerOver*UI*GameObject
+                // notice we don't use on on GetbuttonUp() few lines down, because one can mouse down, move over a UI element and release, which would lead to not lower the isFiring Flag.
+                if (EventSystem.current.IsPointerOverGameObject())
+                {
+                    //	return;
+                }
+
+                if (!this.IsFiring)
+                {
+                    this.IsFiring = true;
+                }
+            }
+
+            if (Input.GetButtonUp("Fire1"))
+            {
+                if (this.IsFiring)
+                {
+                    this.IsFiring = false;
+                }
+            }
+
             if (Input.GetButtonDown("OkImage"))
             {
-                StartCoroutine(ImgAppearDisappear(3, OkImage));
+                if (!IsOkImage)
+                {
+                    IsOkImage = true;
+                }
+            }
+
+            if (Input.GetButtonUp("OkImage"))
+            {
+                if (IsOkImage)
+                {
+                    IsOkImage = false;
+                }
             }
             
             if (Input.GetButtonDown("WrongImage"))
             {
-                StartCoroutine(ImgAppearDisappear(3, WrongImage));
+                if (!isWrongImage)
+                {
+                    isWrongImage = true;
+                }
+            }
+
+            if (Input.GetButtonUp("WrongImage"))
+            {
+                if (isWrongImage)
+                {
+                    isWrongImage = false;
+                }
             }
         }
-        
-        private IEnumerator ImgAppearDisappear(float timespan, GameObject obj)
+        #endregion
+
+        #region IPunObservable implementation
+
+        public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
         {
-            Canvass.transform.position += new Vector3(0, .5f, 0);
-            yield return new WaitForSeconds(timespan);
-            obj.SetActive(false);
-            yield return new WaitForSeconds(timespan);
-            obj.SetActive(true);
+            if (stream.IsWriting)
+            {
+                // We own this player: send the others our data
+                stream.SendNext(this.IsFiring);
+                stream.SendNext(this.Health);
+                stream.SendNext(IsOkImage);
+                stream.SendNext(isWrongImage);
+            }
+            else
+            {
+                // Network player, receive data
+                this.IsFiring = (bool)stream.ReceiveNext();
+                this.Health = (float)stream.ReceiveNext();
+                IsOkImage = (bool)stream.ReceiveNext();
+                isWrongImage = (bool)stream.ReceiveNext();
+            }
         }
 
         #endregion
