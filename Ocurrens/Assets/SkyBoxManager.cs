8378591e7674fa073ee6f6d2f9745257ad0a4da4
Pierre Durand <85309955+TotallyNotAB0t@users.noreply.github.com>
2023-03-31 13:33:56 +0200

    Skybox switch

---
+++ Ocurrens/Assets/SkyBoxManager.cs
@@ -0,0 +1,28 @@
+using Photon.Pun;
+using UnityEngine;
+
+public class SkyBoxManager : MonoBehaviourPun
+{
+    [SerializeField] private Material skybox1;
+    [SerializeField] private Material skybox2;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        RenderSettings.skybox = skybox1;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+            photonView.RPC("SwapSkybox", RpcTarget.AllBuffered);
+        }
+    }
+
+    [PunRPC]
+    private void SwapSkybox()
+    {
+        RenderSettings.skybox = RenderSettings.skybox == skybox1 ? skybox2 : skybox1;
+    }
+}
